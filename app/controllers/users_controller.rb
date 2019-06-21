29bd62729d03759a8c9abd1022a6949c269ed827
Chris <chris.daf@gmail.com>
2019-06-21 14:07:59 +0100

    Add passwords not matching and character length errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,14 @@ class UsersController < ApplicationController
     session[:user] = @user
     if @user.save
       redirect_to posts_url
-    else
-      flash[:error] = "Email address already taken"
+    elsif User.find_by(email: params[:user][:email]).present?
+      flash[:alert] = "Email address already taken"
+      redirect_to users_url
+    elsif @user.password != @user.password_confirmation
+      flash[:alert] = "Passwords do not match"
+      redirect_to users_url
+    else 
+      flash[:alert] = "Passwords must be between 6 & 10 characters"
       redirect_to users_url
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,9 @@ class User < ApplicationRecord
 
   has_secure_password
   validates :email, presence: true, uniqueness: true
+  validates :name, presence: true
+  validates :password, presence: true, :length => { :in => 6..10 }
+  validates :password_confirmation, presence: true, :length => { :in => 6..10 }
+
 
 end
